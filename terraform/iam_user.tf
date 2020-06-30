d96c3e9e09ec0f917f876ed8347f87ba471da18e
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-06-30 12:05:51 -0400

    Remove unnecessary access to GeoIP license key SSM parameter

--- terraform/iam_user.tf
+++ terraform/iam_user.tf
@@ -10,6 +10,6 @@ module "iam_user" {
   }
 
   entity         = "ansible-role-cyhy-commander"
-  ssm_parameters = ["/github/oauth_token", "/cyhy/core/geoip/license_key"]
+  ssm_parameters = ["/github/oauth_token"]
   tags           = var.tags
 }
